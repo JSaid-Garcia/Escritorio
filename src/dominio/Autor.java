419c09221e621770d3b652379a1e98e293163b30
jaine <jaine@JASA>
2025-08-16 11:24:37 -0600

    Segundo commit "Agregue las codigo en las dos clases del paquete domionio para el funcionamiento de la interfaz grafica"

--- src/dominio/Autor.java
+++ src/dominio/Autor.java
@@ -9,7 +9,53 @@ package dominio;
  * @author jainer Said Garcia
  */
 public class Autor {
-  
+    private int idAutor;
+    private String nombre;
+    private String nacionalidad;
+
+    // Constructor
+    public Autor(int idAutor, String nombre, String nacionalidad) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+  /**
+     * @Override Method of toString() of Java Library
+     *  toString Method
+     * @return String
+   **/
+
+    @Override
+    public String toString() {
+        return nombre + " (" + nacionalidad + ")";
+    }
 }
+
+
 
 
--- src/dominio/Libro.java
+++ src/dominio/Libro.java
@@ -9,5 +9,75 @@ package dominio;
  * @author jainer Said Garcia Gonzalez
  */
 public class Libro {
-    
+
+    private int idLibro;
+    private String titulo;
+    private Autor autor;
+    private String genero;
+    private int anno;
+    private String ISBN;
+
+    // Constructor
+    public Libro(int idLibro, String titulo, Autor autor, String genero, int anno, String ISBN) {
+        this.idLibro = idLibro;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anno = anno;
+        this.ISBN = ISBN;
+    }
+
+    public void setIdLibro(int idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public int getIdLibro() {
+        return idLibro;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setAno(int ano) {
+        this.anno = anno;
+    }
+
+    public int getAno() {
+        return anno;
+    }
+
+    public void setIsbn(String isbn) {
+        this.ISBN = ISBN;
+    }
+
+    public String getIsbn() {
+        return ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + autor.getNombre();
+    }
+
 }
